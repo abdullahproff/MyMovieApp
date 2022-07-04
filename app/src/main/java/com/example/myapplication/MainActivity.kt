52a656f288cb96901ee65357de3655e0ffb2e77c
AbdullahAbdullaev <amabdullaevproff@gmail.com>
2022-07-04 23:58:01 +0300

    Added navigation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -45,21 +45,25 @@ class MainActivity : AppCompatActivity() {
 
         bottom_navigation.setOnNavigationItemSelectedListener {
 
-            when (it.itemId) {
-                R.id.favorites -> {
-                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.watch_later -> {
-                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.selections -> {
-                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
+        when (it.itemId) {
+            R.id.favorites -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_placeholder, FavoritesFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
             }
+            R.id.watch_later -> {
+                Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.selections -> {
+                Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
         }
     }
+}
 }

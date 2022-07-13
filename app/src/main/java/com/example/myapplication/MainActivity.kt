1091a9b5b44952538f258638082a641a615904a8
AbdullahAbdullaev <amabdullaevproff@gmail.com>
2022-07-14 02:05:22 +0300

    Added search line

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.amsdevelops.filmssearch.HomeFragment
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {

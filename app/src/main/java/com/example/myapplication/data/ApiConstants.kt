585326f9341955c406a9ef170d3a997ea6f0ac8d
AbdullahAbdullaev <amabdullaevproff@gmail.com>
2022-10-23 16:53:47 +0300

    Merge remote-tracking branch 'origin/Added_tests_and_custom_view' into Added_tests_and_custom_view

--- app/src/main/java/com/example/myapplication/data/ApiConstants.kt
+++ app/src/main/java/com/example/myapplication/data/ApiConstants.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.data
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiConstants {
     const val BASE_URL = "https://api.themoviedb.org/"
     const val IMAGES_URL = "https://image.tmdb.org/t/p/"
